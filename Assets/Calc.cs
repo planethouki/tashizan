922ef90a1aad82e3c0caa1bc512ac3979bb102d3
planethouki <26595148+planethouki@users.noreply.github.com>
2020-04-14 21:02:22 +0900

    UI & Calc

---
+++ Assets/Calc.cs
@@ -0,0 +1,31 @@
+﻿public class Calc
+{
+        public static string Input(string buttonName, string inputText)
+        {
+                string text;
+                if (buttonName == "bs")
+                {
+                        int inputNumber = int.Parse(inputText);
+                        inputNumber /= 10;
+                        text = inputNumber.ToString();
+                }
+                else if (buttonName == "go")
+                {
+                        text = "0";
+                }
+                else
+                {
+                        int inputNumber = int.Parse(inputText);
+                        inputNumber *= 10;
+                        inputNumber += int.Parse(buttonName);
+                        if (inputNumber > 999)
+                        {
+                                inputNumber %= 1000;
+                        }
+
+                        text = inputNumber.ToString();
+                }
+
+                return text;
+        }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    public Text currentSumText;
+    public Text cardText;
+    public Text cardMarkText;
+    public Text inputText;
+    public Text remainText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick(string buttonName)
+    {
+        string result = Calc.Input(buttonName, inputText.text);
+        Debug.Log(result);
+        inputText.text = result;
+    }
+}
